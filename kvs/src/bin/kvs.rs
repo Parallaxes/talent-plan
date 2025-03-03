2c5aa77e837b1987d6e4a3040dacad6c7d9c5a1b
Parallaxes <lucasmasterson@gmail.com>
2025-03-02 16:28:10 -0800

    Complete Project 1

--- kvs/src/bin/kvs.rs
+++ kvs/src/bin/kvs.rs
@@ -1,3 +1,42 @@
+use clap::Command;
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("kvs")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            Command::new("get")
+                .about("Get a value from the store")
+                .arg(clap::Arg::new("key").required(true)),
+        )
+        .subcommand(
+            Command::new("set")
+                .about("Set a value in the store")
+                .arg(clap::Arg::new("key").required(true))
+                .arg(clap::Arg::new("value").required(true)),
+        )
+        .subcommand(
+            Command::new("rm")
+                .about("Remove a value from the store")
+                .arg(clap::Arg::new("key").required(true)),
+        )
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("set", _matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Some(("get", _matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        Some(("rm", _matches)) => {
+            eprintln!("unimplemented");
+            exit(1);
+        }
+        _ => unreachable!(),
+    }
 }
